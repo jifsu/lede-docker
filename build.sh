a9c83853262e727abb9f132908ea63b6d7c923b7
Oskari Rauta <oskari.rauta@gmail.com>
2017-12-11 22:32:45 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,10 @@ set -u
 source config.txt
 
 docker import https://downloads.lede-project.org/releases/$VERSION/targets/x86/64/$REPO-generic-rootfs.tar.gz $DISTRO:$VERSION
-CTID=$(docker run -d -it $DISTRO:$VERSION /sbin/init)
+CTID=$(docker run -d -it $DISTRO:$VERSION /bin/sh)
 docker exec $CTID mkdir -pv /var/run
 docker exec $CTID mkdir -pv /var/lock
 docker exec $CTID opkg update
+docker exec $CTID opkg install nano
 docker commit $CTID $HUBUSER/$DISTRO:$VERSION
 docker push $HUBUSER/$DISTRO:$VERSION
