9b0b1018ab3baab63358d889c3facedd2992cd28
Oskari Rauta <oskari.rauta@gmail.com>
2017-12-12 01:01:25 +0200

    Delete run.sh

--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-source config.txt
-#docker run --privileged -it $HUBUSER/$DISTRO:$VERSION -p 8080:80, 8022:22 /sbin/init
-docker run -p "8080:80" -p "8022:22" --privileged -it $HUBUSER/$DISTRO:$VERSION /sbin/init
