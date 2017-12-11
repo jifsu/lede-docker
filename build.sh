06df2fc3f32d25d2abe13fa5ba5039678dd008a4
Oskari Rauta <oskari.rauta@gmail.com>
2017-12-11 22:21:42 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -u
 source config.txt
 
 docker import https://downloads.lede-project.org/releases/$VERSION/targets/x86/64/$REPO-generic-rootfs.tar.gz $DISTRO:$VERSION
-CTID=$(docker run -d -it $DISTRO:$VERSION sh)
+CTID=$(docker run -d -it $DISTRO:$VERSION /sbin/init)
 docker exec $CTID mkdir -pv /var/run
 docker exec $CTID mkdir -pv /var/lock
 docker exec $CTID opkg update
