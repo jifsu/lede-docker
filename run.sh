6d8f3b8945713c1a59b841b3e24e28c81441455a
Oskari Rauta <oskari.rauta@gmail.com>
2017-12-11 23:21:52 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 source config.txt
-docker run --privileged -it $HUBUSER/$DISTRO:$VERSION /sbin/init
+#docker run --privileged -it $HUBUSER/$DISTRO:$VERSION -p 8080:80, 8022:22 /sbin/init
+docker run -p "8080:80" -p "8022:22" --privileged -it $HUBUSER/$DISTRO:$VERSION /sbin/init
